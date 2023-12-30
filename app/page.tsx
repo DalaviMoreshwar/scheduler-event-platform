20a009be3f418767e6a203eceaa66d4048fdaf39
Moreshwar Dalvi <moreshwardlv@gmail.com>
2023-12-30 23:42:36 +0530

    D

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <main>
-      <Button variant={"destructive"}>Click me</Button>
-    </main>
-  );
-}
