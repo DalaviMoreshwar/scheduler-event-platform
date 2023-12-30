ed653f46269c000d78484321fd117b9e99d8b008
Moreshwar Dalvi <moreshwardlv@gmail.com>
2023-12-30 23:57:53 +0530

    project initial setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-lg">Schedular</h1>
+      <Button variant="destructive">Click me</Button>
+    </main>
+  );
+}
