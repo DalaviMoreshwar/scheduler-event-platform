09e34a5949ff58c1b24d2ccbcb93f9d1d420c11f
Moreshwar Dalvi <moreshwardlv@gmail.com>
2023-12-31 01:05:50 +0530

    page layouts and assets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,5 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-lg">Schedular</h1>
-      <Button variant="destructive">Click me</Button>
-    </main>
-  );
+  return <main></main>;
 }
