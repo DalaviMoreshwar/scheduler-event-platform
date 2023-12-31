1bf3e4c2757b0977465410645a2f366acd75fa4e
Moreshwar Dalvi <moreshwardlv@gmail.com>
2023-12-31 17:13:13 +0530

    footer implementation

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-2 p-1 text-center sm:flex-row">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <p className="">2023 Scheduler. All rights reseved.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
