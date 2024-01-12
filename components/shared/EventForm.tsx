28ca7a688881522a4bc8fd53fc5772c52fa8287b
Moreshwar Dalvi <moreshwardlv@gmail.com>
2024-01-13 00:23:06 +0530

    populated category field as well

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -48,10 +48,12 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
     event && type === "Update"
       ? {
           ...event,
+          categoryId: event.category._id,
           startDateTime: new Date(event.startDateTime),
           endDateTime: new Date(event.endDateTime),
         }
       : eventDefaultValues;
+
   const router = useRouter();
 
   const { startUpload } = useUploadThing("imageUploader");
