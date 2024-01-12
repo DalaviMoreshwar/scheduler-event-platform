4b7cfa0d7e338a82cbe36e1a31ec44a853b0f492
Moreshwar Dalvi <moreshwardlv@gmail.com>
2024-01-12 23:15:55 +0530

    misplaced

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -8,7 +8,7 @@ const UpdateEvent = () => {
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h3-bold text-center sm:text-left">
-          Create Event
+          Update Event
         </h3>
       </section>
 
