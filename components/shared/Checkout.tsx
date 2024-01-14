084ba1e19ed9629b98d49689d31c51fe8ad97b76
Moreshwar Dalvi <moreshwardlv@gmail.com>
2024-01-14 22:01:55 +0530

    basic form config

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Button } from "../ui/button";
+import { IEvent } from "@/lib/database/models/event.model";
 
-const Checkout = () => {
-  return <form></form>;
+const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  const onCheckout = () => {};
+
+  return (
+    <form action={onCheckout} method="post">
+      <Button type="submit" role="link" className="button sm:w-fit">
+        {event.isFree ? "Get Ticket" : "Buy Ticket"}
+      </Button>
+    </form>
+  );
 };
 
 export default Checkout;
