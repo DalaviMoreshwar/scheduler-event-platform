703c3daf625261e9a99fded17829f0487ec66de5
Moreshwar Dalvi <moreshwardlv@gmail.com>
2024-01-18 22:01:46 +0530

    add server actions for orders and display

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,8 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
@@ -9,6 +11,9 @@ const ProfilePage = async () => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
+  const orders = await getOrdersByUser({ userId, page: 1 });
+
+  const orderEvents = orders?.data.map((order: IOrder) => order.event);
   const organizedEvents = await getEventsByUser({ userId, page: 1 });
 
   return (
@@ -25,7 +30,7 @@ const ProfilePage = async () => {
 
       <section className="wrapper my-8">
         <Collection
-          data={[]}
+          data={orderEvents}
           emptyTitle="No events purchased yet."
           emptyStateSubtext="No worries - plenty of excitung events to explore."
           collectionType="My_Tickets"
